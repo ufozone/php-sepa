49fe267f6d769c540a66f114e969ad7a2d186fec
ufozone <dev@ufozone.de>
2024-08-25 17:47:39 +0200

    Fix typos

--- vendor/lib/Sepa/DirectDebit.php
+++ vendor/lib/Sepa/DirectDebit.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace ufozone\phpsepa\Sepa;
 
-use ufozone\phpsepa\Sepa;
+use \ufozone\phpsepa\Sepa;
 use \ufozone\phpsepa\Sepa\Validator\Factory as ValidatorFactory;
 
 /**
